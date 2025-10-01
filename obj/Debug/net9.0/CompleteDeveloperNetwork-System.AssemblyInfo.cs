378d3387d46c47cda1db68d8927f7a5ca9b5e368
Puventhiran Ganeson <puventhiran@graduate.utm.my>
2025-10-01 15:07:38 +0800

    Update
    1/10/2025

--- obj/Debug/net9.0/CompleteDeveloperNetwork-System.AssemblyInfo.cs
+++ obj/Debug/net9.0/CompleteDeveloperNetwork-System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompleteDeveloperNetwork-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97664b8a898b064a2c36dade51239e026f57e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d24b81fd697683730c67db747fa0e56063eeda1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompleteDeveloperNetwork-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompleteDeveloperNetwork-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
